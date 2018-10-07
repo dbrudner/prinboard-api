8dcd90c9bb0039103b0f5ce98fd11f51bd75ea20
dbrudner <dbrudne1@gmail.com>
2018-10-07 12:18:29 -0500

    updated server.js for mongoose

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes")(app);
-const port = 3001;
+const port = 3000;
+const developmentUrl = "mongodb://localhost/prinboard";
+
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+} else {
+	mongoose.connect(
+		developmentUrl,
+		err => {
+			if (err) {
+				throw err;
+			} else {
+				console.log("connected");
+			}
+		}
+	);
+}
 
 app.listen(port, () => console.log(`Listening on ${port}`));
