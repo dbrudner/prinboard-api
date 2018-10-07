fe4d9493c8e30513dc1c8b39a69b642d472ac809
dbrudner <dbrudne1@gmail.com>
2018-10-07 13:38:37 -0500

    adding dev port back in

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes")(app);
-const port = process.env.PORT;
+const port = 3000 || process.env.PORT;
 const developmentUrl = "mongodb://localhost/prinboard";
 
 if (process.env.MONGODB_URI) {
