44f3d269783d24f17faf36b441ff1936969993fb
dbrudner <dbrudne1@gmail.com>
2018-10-07 10:27:00 -0500

    initial routes commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+	app.get("/api/posts", (req, res) => {});
+
+	app.post("/api/new", (req, res) => {});
+
+	app.post("/api/update/:id", (req, res) => {});
+
+	app.get("/api/delete/:id", (req, res) => {});
+};
