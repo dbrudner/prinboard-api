8cd143ee8c2f9635830c6ffb0b8e4fc3269d4439
dbrudner <dbrudne1@gmail.com>
2018-10-07 12:59:32 -0500

    added total results with async queries to search route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const db = require("../schema");
 const bodyParser = require("body-parser");
+const async = require("async");
+const db = require("../schema");
 
 module.exports = app => {
 	app.use(bodyParser());
 
-	app.get("/api/posts/search", (req, res) => {
+	app.get("/api/search", (req, res) => {
 		const { page, query } = req.query;
 		const searchQuery = {
 			$text: { $search: query }
@@ -12,27 +13,38 @@ module.exports = app => {
 
 		db.Link.createIndexes({ author: "text", name: "text" });
 
-		db.Link.find(searchQuery)
-			.skip(page)
-			.limit(10)
-			.exec((err, results) => {
+		const paginatedResultsQuery = cb => {
+			db.Link.find(searchQuery)
+				.skip(parseInt(page) || 0)
+				.limit(5)
+				.exec((err, results) => {
+					if (err) {
+						return cb(err);
+					}
+
+					cb(null, { results, query: req.query });
+				});
+		};
+
+		const countQuery = cb => {
+			db.Link.countDocuments(query, (err, count) => {
 				if (err) {
-					throw err;
+					return cb(err);
 				}
-
-				console.log(results);
-
-				const totalResults = results.length;
-				res.json({ results, query });
+				cb(null, count);
 			});
+		};
+
+		async.parallel([paginatedResultsQuery, countQuery], (err, results) => {
+			if (err) throw err;
+			res.json({ ...results[0], totalHits: results[1] });
+		});
 	});
 
 	app.post("/api/new", (req, res) => {
-		console.log(req.body);
-
 		db.Link.create(req.body, (err, newLink) => {
 			if (err) {
-				console.log(err);
+				throw err;
 			} else {
 				res.json(newLink);
 			}
@@ -41,6 +53,7 @@ module.exports = app => {
 
 	app.post("/api/update", (req, res) => {
 		const { _id, ...update } = req.body;
+
 		db.Link.findOneAndUpdate(
 			{ _id },
 			{ ...update, updated_at: new Date() },
@@ -56,6 +69,7 @@ module.exports = app => {
 
 	app.get("/api/delete/:_id", (req, res) => {
 		const { _id } = req.params;
+
 		db.Link.findOneAndDelete({ _id }, err => {
 			if (err) {
 				throw err;
