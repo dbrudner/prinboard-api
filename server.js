ab0d0d3329a36088eab76314ac0d2bb7aa81e503
dbrudner <dbrudne1@gmail.com>
2018-10-07 13:43:58 -0500

    fixed port in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes")(app);
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const developmentUrl = "mongodb://localhost/prinboard";
 
 if (process.env.MONGODB_URI) {
