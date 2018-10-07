64c2ee06d4c5f404a79e79a2254e6eec1083f674
dbrudner <dbrudne1@gmail.com>
2018-10-07 13:28:00 -0500

    removing explicit port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes")(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const developmentUrl = "mongodb://localhost/prinboard";
 
 if (process.env.MONGODB_URI) {
