0758feaee9bf7d14b69abd3d3a30e9c48d0d31c4
dbrudner <dbrudne1@gmail.com>
2018-10-10 18:08:00 -0500

    fixing cors

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ module.exports = app => {
 	// Add headers
 	app.use(function(req, res, next) {
 		// Website you wish to allow to connect
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+		res.setHeader("Access-Control-Allow-Origin", "*");
 
 		// Request methods you wish to allow
 		res.setHeader(
@@ -21,11 +21,6 @@ module.exports = app => {
 			"X-Requested-With,content-type"
 		);
 
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader("Access-Control-Allow-Credentials", true);
-
-		// Pass to next layer of middleware
 		next();
 	});
 
