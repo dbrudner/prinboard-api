5cdebaeb6be477cc65cd95e0d5facd9000bac71f
dbrudner <dbrudne1@gmail.com>
2018-10-10 19:58:15 -0500

    rewriting and renaming routes. Fixed fetch links to handle fetching links without searching

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const async = require("async");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const db = require("../schema");
 
 module.exports = app => {
@@ -12,65 +11,39 @@ module.exports = app => {
 
 	app.use(bodyParser());
 
-	app.get("/api/links", (req, res) => {
-		const { page } = req.query;
-		const paginatedResultsQuery = cb => {
-			db.Link.find()
-				.sort({ created_at: -1 })
-				.skip((parseInt(page - 1) || 0) * 5)
-				.limit(5)
-				.exec((err, results) => {
-					if (err) {
-						res.status(500).send("Something went wrong");
-					}
-
-					cb(null, { results, query: req.query });
-				});
-		};
-
-		const countQuery = cb => {
-			db.Link.countDocuments({}, (err, count) => {
-				if (err) {
-					res.status(500).send("Something went wrong");
-				}
-
-				cb(null, count);
-			});
-		};
-
-		async.parallel([paginatedResultsQuery, countQuery], (err, results) => {
-			if (err) {
-				res.status(500).send("Something went wrong");
-			}
-
-			res.json({ ...results[0], totalHits: results[1] });
-		});
-	});
-
-	app.get("/api/search", (req, res) => {
+	app.get("/api/fetch", (req, res) => {
 		const { page, query } = req.query;
-		const searchQuery = {
-			$text: { $search: query || "" }
+		const createSearchQuery = () => {
+			if (query) {
+				return {
+					$text: { $search: query }
+				};
+			}
+			return {};
 		};
 
 		const paginatedResultsQuery = cb => {
-			db.Link.find(searchQuery)
+			db.Link.find(createSearchQuery())
 				.sort({ created_at: -1 })
 				.skip((parseInt(page - 1) || 0) * 5)
 				.limit(5)
-				.exec((err, results) => {
+				.exec((err, links) => {
+					const result = query
+						? { links, query: req.query }
+						: { links };
+
 					if (err) {
-						res.status(500).send("Something went wrong");
+						cb(err);
 					}
 
-					cb(null, { results, query: req.query });
+					cb(null, result);
 				});
 		};
 
 		const countQuery = cb => {
-			db.Link.countDocuments(searchQuery, (err, count) => {
+			db.Link.countDocuments(createSearchQuery(), (err, count) => {
 				if (err) {
-					res.status(500).send("Something went wrong");
+					cb(err);
 				}
 
 				cb(null, count);
@@ -86,11 +59,14 @@ module.exports = app => {
 		});
 	});
 
-	app.post("/api/new", (req, res) => {
+	app.post("/api/create", (req, res) => {
 		db.Link.create(req.body, (err, newLink) => {
+			// Leave this console.log for checking post bodies inside heroku logs if something goes wrong
+			// Replace this with morgan or another logger at some point
+			console.log("Hey?");
+			console.log(req.body);
 			if (err) {
-				// Leave this console.log for checking post bodies inside heroku logs if something goes wrong
-				console.log(req.body);
+				throw err;
 				res.status(500).send("Something went wrong");
 			}
 			res.json(newLink);
@@ -128,7 +104,7 @@ module.exports = app => {
 		});
 	});
 
-	app.get("/api/tags", (req, res) => {
+	app.get("/api/fetch/tags", (req, res) => {
 		db.Link.find().distinct("tags", (err, tags) => {
 			if (err) {
 				res.status(500).send("Something went wrong");
